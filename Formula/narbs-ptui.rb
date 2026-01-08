2f161b01f86e3acac3ecdd60b78fb68e5a865d1f
Christian Clare <narbs@narbs.net>
2026-01-07 21:09:31 -0500

    Update ptui to v1.0.12

--- Formula/narbs-ptui.rb
+++ Formula/narbs-ptui.rb
@@ -1,8 +1,8 @@
 class NarbsPtui < Formula
   desc "PTUI"
   homepage "https://github.com/narbs/ptui"
-  url "https://github.com/narbs/ptui/archive/refs/tags/v1.0.9.tar.gz"
-  sha256 "c85a7c39b4fb2774db7010d2e18771bb46c52d7ae65f411d4096facc2b68269d"
+  url "https://github.com/narbs/ptui/archive/refs/tags/v1.0.12.tar.gz"
+  sha256 "f836a7c8ea8466bc34f0e67b3c7d16a1f4ddd551a1b5ff558357db623de40a72"
   license "MIT"
 
   depends_on "rust" => :build
