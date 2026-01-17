e9f5b5ad7e0b739445438498433cb1e2372e4f62
Christian Clare <narbs@narbs.net>
2026-01-16 19:36:54 -0500

    Update ptui to v2.0.0

--- Formula/narbs-ptui.rb
+++ Formula/narbs-ptui.rb
@@ -1,16 +1,17 @@
 class NarbsPtui < Formula
   desc "PTUI"
   homepage "https://github.com/narbs/ptui"
-  url "https://github.com/narbs/ptui/archive/refs/tags/v1.0.12.tar.gz"
-  sha256 "f836a7c8ea8466bc34f0e67b3c7d16a1f4ddd551a1b5ff558357db623de40a72"
+  url "https://github.com/narbs/ptui/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "dde212b2654dce1e77039a0c6c8993b7ff7d6af3f72f9bc134cf295991fea6f6"
   license "MIT"
 
   depends_on "rust" => :build
+  depends_on "nasm" => :build
   depends_on "chafa"
   depends_on "imagemagick"
 
   def install
-    system "cargo", "install", *std_cargo_args
+    system "cargo", "install", "--features", "fast-jpeg", *std_cargo_args
   end
 
   test do
